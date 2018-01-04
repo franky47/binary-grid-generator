b58137cd434f4a8f4518f458e14c1f555cb95eba
Francois Best <francoisbest@gmail.com>
2018-01-04 07:45:12 +0100

    feat: Update from latest webtask revision

--- server/index.js
+++ server/index.js
@@ -6,7 +6,26 @@ const generateMatrix = (side, density) => {
   return new Promise((resolve) => {
     const size = side * side
     const numOnes = Math.floor(size * density)
+
+    // Generate blank matrix
     const matrix = Array(side).fill([]).map(() => Array(side).fill(0))
+
+    if (numOnes === 0) {
+      return resolve(matrix)
+    } else if (numOnes === size) {
+      return resolve(Array(side).fill([]).map(() => Array(side).fill(1)))
+    }
+
+    // To know where to insert the ones, consider a 3 x 3 matrix,
+    // and assign a coordinate number to each cell:
+    // [0, 1, 2]
+    // [3, 4, 5]
+    // [6, 7, 8]
+    // The deal is to generate an array of N unique random values
+    // with a uniform progression between 0 and side^2 - 1
+
+    // Hack: to avoid array reduction while deduping,
+    // generate a larger one initially, dedupe and keep a subset.
     take(uniq(pd.rint(numOnes * 4, 0, size - 1)), numOnes)
       .forEach(c => {
         const x = Math.floor(c % side)
@@ -21,6 +40,12 @@ const stringifyMatrix = (matrix) => {
   return matrix.map((row) => row.join('')).join('|')
 }
 
+const getMatrixDensity = (matrix) => {
+  const length = matrix.length * matrix.length
+  const reducer = (acc, val) => acc + val
+  return matrix.reduce((acc, row) => acc + row.reduce(reducer), 0) / length
+}
+
 /**
 * @param context {WebtaskContext}
 *
@@ -29,19 +54,31 @@ const stringifyMatrix = (matrix) => {
 * @query side {Integer} - The size of the square (side x side).
 */
 module.exports = function (context, cb) {
-  const numMatrices = parseInt(context.query.num_matrices) || 100
+  const size = parseInt(context.query.size) || 100
   const side = parseInt(context.query.side) || 5
+  const mindp = parseFloat(context.query.min_density_probability || '0.3')
+  const maxdp = parseFloat(context.query.max_density_probability || '0.7')
+  const distribution = context.query.distribution || 'uniform'
 
-  // Get N numbers between 0.0 and 1.0 with uniform distribution,
-  // between 30% and 70%:
-  const densities = pd.runif(numMatrices, 0.3, 0.7)
+  console.log(`Request: ${size} ${side}x${side} matrices of ${distribution} density [${mindp};${maxdp}]`)
+
+  var densities = []
+  switch (distribution) {
+    case 'uniform':
+      densities = pd.runif(size, mindp, maxdp)
+      break
+    case 'normal':
+      densities = pd.rnorm(size, 0.5, 0.25 * (maxdp - mindp))
+        .map(d => Math.max(Math.min(d, maxdp), mindp))
+      break
+  }
 
   // Generate the matrices
   return Promise.all(densities.map((d) => generateMatrix(side, d)))
     .then((matrices) => {
       cb(null, {
         matrices: matrices.map(m => stringifyMatrix(m)),
-        densities
+        densities: matrices.map(m => getMatrixDensity(m))
       })
     })
 }

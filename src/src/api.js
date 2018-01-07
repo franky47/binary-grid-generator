cef1f872062cd25f3ee712d4e63bc2978871b65e
Francois Best <francoisbest@gmail.com>
2018-01-07 18:02:05 +0100

    feat: Push results to API

--- src/src/api.js
+++ src/src/api.js
@@ -1,21 +1,20 @@
 import axios from 'axios'
 import { decodeMatrix } from './utility'
 
-const url = 'https://wt-92cccbcf027a1b4070443ff04b9033cc-0.run.webtask.io/binary-grid-generator'
-
 const defaultParams = {
   side: 5,
   size: 10
 }
 
 const get = (params = defaultParams) => {
+  const url = 'https://wt-92cccbcf027a1b4070443ff04b9033cc-0.run.webtask.io/binary-grid-generator'
   return axios.get(url, params)
     .then((response) => response.data.matrices.map(decodeMatrix))
 }
 
 const pushResult = (result) => {
-  console.log(result)
-  // return axios.post(url + '/push-result', result)
+  const url = 'https://wt-92cccbcf027a1b4070443ff04b9033cc-0.run.webtask.io/bgg-calibration-push-result'
+  return axios.post(url, result)
 }
 
 export default { get, pushResult }

672f8043db09598e6df07f90f67abe5a0101f66c
Francois Best <francoisbest@gmail.com>
2018-01-07 18:02:49 +0100

    feat: Styling

--- src/src/screens/calibration/Intro.js
+++ src/src/screens/calibration/Intro.js
@@ -1,13 +1,11 @@
 import React from 'react'
 
-import './Intro.css'
-
 export default class Intro extends React.PureComponent {
   render () {
     const { loading } = this.props
     return (
       <section className='intro'>
-        <h1>Binary Grid <span className='separator'>|</span> Calibration</h1>
+        <h1>Binary Grid <span className='secondary'>|</span> Calibration</h1>
         <p>Todo: add descriptive text here.</p>
         { loading &&
           <p>Loading</p>
--- src/src/screens/calibration/Summary.js
+++ src/src/screens/calibration/Summary.js
@@ -12,8 +12,8 @@ export default class Summary extends React.PureComponent {
         <p>Best time: <b>{bestTime.toFixed(3)}s</b></p>
         <p>Worst time: <b>{worstTime.toFixed(3)}s</b></p>
         <p>Average time: <b>{averageTime.toFixed(3)}s</b></p>
-        <p>Completion rate: <b>{completionRate}%</b> ({numCompleted} / {numTotal})</p>
-        <p>Success rate: <b>{successRate}%</b> ({numCorrect} / {numCompleted})</p>
+        <p>Completion rate: <b>{completionRate}%</b> <span className='secondary'>({numCompleted} / {numTotal})</span></p>
+        <p>Success rate: <b>{successRate}%</b> <span className='secondary'>({numCorrect} / {numCompleted})</span></p>
         { /* todo: add field to leave a comment */ }
         {/* <button>Play again</button> */}
       </section>

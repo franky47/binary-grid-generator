e8143b9544c9118dd45ea18773dd522fbce79370
Francois Best <francoisbest@gmail.com>
2018-01-07 22:43:26 +0100

    fix: Remove separator on mobile

--- src/src/screens/calibration/Intro.js
+++ src/src/screens/calibration/Intro.js
@@ -1,12 +1,13 @@
 import React from 'react'
+import './Intro.css'
 
 export default class Intro extends React.PureComponent {
   render () {
     const { loading } = this.props
     return (
       <section className='intro'>
-        <h1>Binary Grid <span className='secondary'>|</span> Calibration</h1>
-        <p>Todo: add descriptive text here.</p>
+        <h1>Binary Grid <span className='secondary separator'>|</span> Calibration</h1>
+        <p>Count the ones as fast as you can !</p>
         { loading &&
           <p>Loading</p>
         }

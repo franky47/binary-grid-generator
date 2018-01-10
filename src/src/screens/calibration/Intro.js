6d868a6d0f6edb38da3d9d1a4619bfbe0d7076fd
penny89 <35299334+penny89@users.noreply.github.com>
2018-01-10 14:32:57 +0100

    Update Intro.js

--- src/src/screens/calibration/Intro.js
+++ src/src/screens/calibration/Intro.js
@@ -6,13 +6,13 @@ export default class Intro extends React.PureComponent {
     const { loading } = this.props
     return (
       <section className='intro'>
-        <h1>Binary Grid <span className='secondary separator'>|</span> Calibration</h1>
-        <p>Count the ones as fast as you can !</p>
+        <h1>Matrices binaires <span className='secondary separator'>|</span> Calibration</h1>
+        <p>Compter le nombre de "1" : !</p>
         { loading &&
-          <p>Loading</p>
+          <p>Chargement</p>
         }
         { !loading &&
-          <button onClick={this.props.next}>Start</button>
+          <button onClick={this.props.next}>Commencer</button>
         }
       </section>
     )

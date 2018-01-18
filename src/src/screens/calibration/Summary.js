41d608dd71bc7e23dbd5938a7f56bea2416ca8df
Francois Best <francoisbest@gmail.com>
2018-01-18 10:11:38 +0100

    fix: Typo

--- src/src/screens/calibration/Summary.js
+++ src/src/screens/calibration/Summary.js
@@ -12,7 +12,7 @@ export default class Summary extends React.PureComponent {
         <p>Meilleur temps : <b>{bestTime.toFixed(2)}s</b></p>
         <p>Pire temps : <b>{worstTime.toFixed(2)}s</b></p>
         <p>Temps moyen : <b>{averageTime.toFixed(2)}s</b></p>
-        <p>Temps cumulé : <b>{totalTime.tofixed(2)}s</b></p>  
+        <p>Temps cumulé : <b>{totalTime.toFixed(2)}s</b></p>
         <p>Pourcentage terminé : <b>{completionRate}%</b> <span className='secondary'>({numCompleted} / {numTotal})</span></p>
         <p>Pourcentage réussi : <b>{successRate}%</b> <span className='secondary'>({numCorrect} / {numCompleted})</span></p>
         { /* todo: add field to leave a comment */ }

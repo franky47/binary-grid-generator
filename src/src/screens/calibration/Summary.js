402e038baea3ab53ee9cd5ec2729636ba79caa2e
penny89 <35299334+penny89@users.noreply.github.com>
2018-01-18 09:49:34 +0100

    Update Summary.js

--- src/src/screens/calibration/Summary.js
+++ src/src/screens/calibration/Summary.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 
 export default class Summary extends React.PureComponent {
   render () {
-    const { bestTime, worstTime, averageTime, numCompleted, numTotal, numCorrect } = this.props
+    const { bestTime, worstTime, averageTime, totalTime, numCompleted, numTotal, numCorrect } = this.props
     const completionRate = ((numCompleted / numTotal) * 100).toFixed()
     const successRate = ((numCorrect / numCompleted) * 100).toFixed()
     return (
@@ -12,6 +12,7 @@ export default class Summary extends React.PureComponent {
         <p>Meilleur temps : <b>{bestTime.toFixed(2)}s</b></p>
         <p>Pire temps : <b>{worstTime.toFixed(2)}s</b></p>
         <p>Temps moyen : <b>{averageTime.toFixed(2)}s</b></p>
+        <p>Temps cumulé : <b>{totalTime.tofixed(2)}s</b></p>  
         <p>Pourcentage terminé : <b>{completionRate}%</b> <span className='secondary'>({numCompleted} / {numTotal})</span></p>
         <p>Pourcentage réussi : <b>{successRate}%</b> <span className='secondary'>({numCorrect} / {numCompleted})</span></p>
         { /* todo: add field to leave a comment */ }
@@ -25,6 +26,7 @@ Summary.propTypes = {
   bestTime: PropTypes.number,
   worstTime: PropTypes.number,
   averageTime: PropTypes.number,
+  totalTime: PropTypes.number,
   numCompleted: PropTypes.number,
   numTotal: PropTypes.number,
   numCorrect: PropTypes.number
